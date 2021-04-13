a2fd57030b8b466235b324c8ad22b251806e2037
BluABK <bluabk@bluabk.net>
2021-04-13 14:45:38 +0200

    End of Day , mostly functional state.

---
+++ weeklies/week06/scripts/main.js
@@ -0,0 +1,53 @@
+// Model
+const morseCodeArray =  [ '.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....',
+                          '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.',
+                          '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-',
+                          '-.--', '--..', ' ', '·−·−', '−−−·', '·−−·−']; //4 siste er space, æ ,ø og å
+let encodedString = "";
+const ASCII_OFFSET = 65;
+
+// View
+function updateView() {
+    document.getElementById('app').innerHTML = `
+        <input type="text" onChange="userInput(this.value)" />
+        <div>Out: ${encodedString}</div>
+    `;
+}
+
+// Controller
+function encodeToMorseCode(c) {
+    let charToEncode = c.toUpperCase();
+    let result = "";
+    let morseIndex = 0;
+
+    if (charToEncode == ' ') {
+        morseIndex = morseCodeArray.length -4;
+    } else if (charToEncode == 'Æ') {
+        morseIndex = morseCodeArray.length -3;
+    } else if (charToEncode == 'Ø') {
+        morseIndex = morseCodeArray.length -2;
+    } else if (charToEncode == 'Å') {
+        morseIndex = morseCodeArray.length -1;
+    } else {
+        morseIndex = charToEncode.charCodeAt(0) - ASCII_OFFSET;
+    }
+
+
+    result = morseCodeArray[morseIndex];
+
+    return result;
+}
+
+function userInput(s) {
+    encodedString = "";
+
+    for (let char of s) {
+        encodedString += encodeToMorseCode(char);
+    }
+
+    updateView();
+}
+
+encodeToMorseCode("a");
+
+updateView();
