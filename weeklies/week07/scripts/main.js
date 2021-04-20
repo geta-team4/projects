dd051aa77e62b41a83fcdfeb6e1f6c63b68c485a
BluABK <bluabk@bluabk.net>
2021-04-20 14:39:26 +0200

    Player is attackble and can't underflow HP

--- weeklies/week07/scripts/main.js
+++ weeklies/week07/scripts/main.js
@@ -3,6 +3,11 @@ const BOSS_HP_TOTAL = 500;
 const PLAYER_HP_TOTAL = 350;
 let bossHP = 500;
 let playerHP = 350;
+let playerHasWon = false;
+let bossDmgMultiplier;
+let bossCritDmgMultiplier;
+let playerDmgMultiplier;
+let playerCritDmgMultiplier;
 
 // View
 function updateView() {
@@ -18,6 +23,7 @@ function updateView() {
         <div class="healthbar player-healthbar">
             <p>${playerHP}/${PLAYER_HP_TOTAL}</p>
             <div id="player-health" class="health" style="width: ${playerHP}px;"></div>
+            <div id="player-health-lost" class="health-lost" style="width: ${PLAYER_HP_TOTAL-playerHP}px;"></div>
         </div>
         <br/><br/>
         <button class="attack-button" onClick="attackBoss()">Attack <img src="./img/sword.png" height="20px"></button>
@@ -25,16 +31,38 @@ function updateView() {
 }
 // Controller
 
-function bossAttackPlayer() {
+function bossAttackPlayer(attackDmg) {
+    if (attackDmg <= 0) return;
+    if (bossHP <= 0) return;
+    // Prevent underflow.
+    if (playerHP <= attackDmg) {
+        attackDmg = playerHP;
+        playerHasLost = true;
+    }
+    
+    // Boss deals damage to player.
+    playerHP -= attackDmg;
+    
     updateView();
 }
 
 function attackBoss() {
-    let attackDmg = 42;
+    if (playerHP <= 0) return;
+    let attackDmg = 42; // FIXME: Randomise! Math.random() (max - min)
+
+    // Prevent underflow.
     if (bossHP <= attackDmg) {
         attackDmg = bossHP;
+        playerHasWon = true;
     }
+
+    // Player deals damage to boss.
     bossHP -= attackDmg;
+
+    // Boss deal damage to player.
+    bossAttackPlayer(attackDmg-30);
+    
+
     updateView();
 }
 
